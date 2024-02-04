990adb23c7caa40415626464588dd4db072a43d7
arifuddinraihan <mdarifuddinraihan@gmail.com>
2024-02-04 18:04:07 +0600

    added ARSelect component

--- src/components/form/ARForm.tsx
+++ src/components/form/ARForm.tsx
@@ -1,3 +1,4 @@
+import { Form } from "antd";
 import { ReactNode } from "react";
 import {
   FieldValues,
@@ -26,7 +27,9 @@ const ARForm = ({ onSubmit, children, defaultValues }: TARFormProps) => {
 
   return (
     <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit(onSubmit)}>{children}</form>
+      <Form layout="vertical" onFinish={methods.handleSubmit(onSubmit)}>
+        {children}
+      </Form>
     </FormProvider>
   );
 };
--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,17 +1,24 @@
-import { Button } from "antd";
+import { Button, Col, Flex } from "antd";
 import ARForm from "../../../components/form/ARForm";
 import ARInput from "../../../components/form/ARInput";
 import { FieldValues, SubmitHandler } from "react-hook-form";
+import ARSelect from "../../../components/form/ARSelect";
 
 const CreateAcademicSemester = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     console.log(data);
   };
   return (
-    <ARForm onSubmit={onSubmit}>
-      <ARInput type="text" label="Something" name="Something" />
-      <Button htmlType="submit">Submit</Button>
-    </ARForm>
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <ARForm onSubmit={onSubmit}>
+          <ARInput type="text" label="Semester" name="semester" />
+          <ARInput type="text" label="Year" name="year" />
+          <ARSelect label="name" />
+          <Button htmlType="submit">Submit</Button>
+        </ARForm>
+      </Col>
+    </Flex>
   );
 };
 
