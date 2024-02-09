8328cd28555a56df2751af5f953accdad7241c17
arifuddinraihan <mdarifuddinraihan@gmail.com>
2024-02-09 23:19:35 +0600

    created pages for course management and also created table for faculty list and admin list

---
+++ src/pages/admin/userManagement/AdminTable.tsx
@@ -0,0 +1,9 @@
+const AdminTable = () => {
+  return (
+    <div>
+      <h1>This is AdminTable component</h1>
+    </div>
+  );
+};
+
+export default AdminTable;
---
+++ src/pages/admin/userManagement/FacultyTable.tsx
@@ -0,0 +1,9 @@
+const FacultyTable = () => {
+  return (
+    <div>
+      <h1>This is FacultyTable component</h1>
+    </div>
+  );
+};
+
+export default FacultyTable;
